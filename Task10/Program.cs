2b9a8efadb15808fc5f4ec865c7faf77030892e7
Ekaterina <katya-tuvaeva@yandex.ru>
2023-01-23 16:54:14 +0300

    решила задачу 10

---
+++ Task10/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+int GetNumber()
+{
+    Console.WriteLine("Введите трехзначное число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int resultNumber = GetNumber();
+int result = ((resultNumber / 10) % 10);
+Console.WriteLine (result);
