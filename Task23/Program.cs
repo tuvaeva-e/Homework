0d6f1e8ccaa676f29bc47ff1d5e84f436c1071c3
Ekaterina <katya-tuvaeva@yandex.ru>
2023-01-25 17:38:29 +0300

    решила задачу 23

---
+++ Task23/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Напишите программу, которая принимает на вход число (N) 
+и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+{
+    Console.Write($"{i*i*i},");
+}
