718db575e35c819c9f419011216eb5275114e1de
Ekaterina <katya-tuvaeva@yandex.ru>
2023-02-15 17:52:25 +0300

    решила задачу 54

---
+++ Task54/Program.cs
@@ -0,0 +1,89 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2 */
+
+int GetNumber(string message)
+{
+    int result = 0;
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не корректное число. Повторите вввод ");
+        }
+    }
+    return result;
+}
+
+int[,] InitMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i,j] = (i+j);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+void RegularizeRows(int[,] matrix)
+{
+    int temp = 0;
+    bool flag = true;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        flag=true;
+        while (flag)
+        {
+            flag = false;
+            for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+            {
+                if (matrix[i, j] > matrix[i, j + 1])
+                {
+                    temp = matrix[i, j];
+                    matrix[i, j] = matrix[i, j + 1];
+                    matrix[i, j + 1] = temp;
+                    flag = true;
+                }
+
+            }
+        }
+    }
+}
+
+int rows = GetNumber("Введите кол-во строк:");
+int columns = GetNumber("Введите кол-во столбцов:");
+int[,] matrix = InitMatrix(rows, columns);
+PrintMatrix(matrix);
+Console.WriteLine();
+RegularizeRows(matrix);
+PrintMatrix(matrix);
